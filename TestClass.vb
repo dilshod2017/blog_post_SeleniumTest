02f735eaafd04822be1b231cda8152f20e479549
Dilshod <dilshod20017@git.com>
2020-08-03 13:08:33 -0400

    first commit

---
+++ TestClass.vb
@@ -0,0 +1,18 @@
+﻿Option Explicit On
+' NUnit 3 tests
+' See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+Imports System
+Imports NUnit.Framework
+Namespace BlogPost_test
+
+    <TestFixture>
+    Public Class TestClass
+
+        <Test>
+        Public Sub TestMethod()
+
+        End Sub
+
+    End Class
+
+End Namespace
